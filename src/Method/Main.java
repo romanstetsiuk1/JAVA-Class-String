5345f1bc7fa70d372888fac48026d9147f8b3e75
Roman Stetsiuk <romansteciuk021@gmail.com>
2018-11-06 19:25:48 +0100

    Add codePointCount method

--- src/Method/Main.java
+++ src/Method/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
 //        1. length:
-        String object_1 = new String("Hello ");
-        String object_1a = new String ();
+        String object_1 = "Hello ";
+        String object_1a;
         object_1a = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore " +
                 "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
                 "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
@@ -17,14 +17,14 @@ public class Main {
         System.out.println("----------------------------------------------");
 
 //        2. isEmpty
-        String object_2 = new String();
+        String object_2 = "";
         System.out.println("\nObject 2 is empty? = " + object_2.isEmpty());
         System.out.println("Object 1 is empty? = " + object_1.isEmpty() + "\n");
         System.out.println("----------------------------------------------");
 
 //        3. charAt
 //        Returns the char value at the specified index.
-        String object_3 = new String("Java.");
+        String object_3 = "Java.";
         char obj_3 = object_3.charAt(2);
         char obj_1a = object_1a.charAt(0);
         char obj_1 = object_1.charAt(2);
@@ -43,11 +43,18 @@ public class Main {
 
 //        5. codePointBefore
 //        Return the character (Unicode before point) before the specified index.
-        String object_5 = new String("LIKE!");
+        String object_5 = "LIKE!";
         int obj_5 = object_5.codePointBefore(2);
         System.out.println("In word " + object_5 + "index before nr 2: its index with number = " + obj_5);
         System.out.println("----------------------------------------------");
 
+//        6. codePointCount
+//        Return the number of Unicode code points in the specified text range of this String
+        String object_6 = "codePointCount method use parameters: beginIndex (the first char of the text range) " +
+                "and endIndex (the index after last char)";
+        int codePointIndexByObj_6 = object_6.codePointCount(8,16);
+        System.out.println("codePointCount by object_6 from index 8 by index 16 is: " + codePointIndexByObj_6);
+        System.out.println("----------------------------------------------");
 
 
     }
